7f08cfbea2fbd1f645b30827350efc5d75fcda25
hunglq <hungleip9@gmail.com>
2021-11-15 17:58:03 +0700

    Fix bug

---
+++ src/modules/request/components/popup/AccountStatement.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="account-statement">Account statement</div>
+</template>
+<script lang="ts">
+</script>
+<style scoped lang="scss"></style>
---
+++ src/modules/request/components/popup/PopupwithDrawRequest.vue
@@ -0,0 +1,8 @@
+<template>
+  <base-popup name="popup-withdraw-request" class="popup-withdraw-request">
+    <div>popup-withdraw-request</div>
+  </base-popup>
+</template>
+<script lang="ts">
+</script>
+<style scoped lang="scss"></style>
