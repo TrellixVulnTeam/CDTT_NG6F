daa68141835339d913e3a1488946bdddf7cfda0a
Thanh Nguyen <thanhnt198@gmail.com>
2021-11-17 09:57:44 +0700

    commit

--- src/modules/balance/view/Balance.vue
+++ src/modules/balance/view/Balance.vue
@@ -196,8 +196,8 @@
   span {
     width: 100%;
     display: inline-block;
-    font-size: 0;
-    line-height: 0;
+    font-size: 16px;
+    line-height: 24px;
   }
   .bo-kyc {
     box-shadow: 0px 0.3px 0.9px rgba(0, 0, 0, 0.1), 0px 1.6px 3.6px rgba(0, 0, 0, 0.13);
