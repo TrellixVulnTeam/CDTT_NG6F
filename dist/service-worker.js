c92d2dcd1edd52328bb3d934319bd1e752fa682a
Datnq <dat@beedu.vn>
2021-11-19 13:15:30 +0700

    :rocket: build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e32d947b23419f8ba00a4880d87c9b97.js"
+  "/precache-manifest.0063cf071c37968477f5b3a510186f22.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blockchain-web-backoffice"});
