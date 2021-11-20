3c46e455c741d98841490f03d999e2b829ec6f47
Datnq <dat@beedu.vn>
2021-11-20 13:38:11 +0700

    fix format

--- src/configs/format/index.ts
+++ src/configs/format/index.ts
@@ -58,7 +58,7 @@ export function convertAmount8digit(amount: string | number): string {
 }
 
 export function convertAmountDecimal(amount: string | number, currency: string): string {
-  // if (!amount) return '0'
+  if (!amount) return '0'
   const objConvert = {
     LYNK: 2,
     CLM: 2,
@@ -247,16 +247,15 @@ export function formatNumberPhone(numberPhone: string | null): string {
   } else return ''
 }
 
-export function formatType(type:string|null):string{
- if (type){
-   var arrStr=type.split("_");
-   var strTotal=""
-   arrStr.map((value,i)=>{
-     var str:string=value.substr(0,1);
-     var str2:string=value.substring(1)
-     strTotal+=str.toUpperCase()+str2.toLowerCase()+" ";
-   })
-   return strTotal;
- }else return ""
+export function formatType(type: string | null): string {
+  if (type) {
+    const arrStr = type.split('_')
+    let strTotal = ''
+    arrStr.map((value, i) => {
+      const str: string = value.substr(0, 1)
+      const str2: string = value.substring(1)
+      strTotal += str.toUpperCase() + str2.toLowerCase() + ' '
+    })
+    return strTotal
+  } else return ''
 }
-
