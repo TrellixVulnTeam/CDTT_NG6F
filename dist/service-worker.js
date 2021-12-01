d89bd4a350fef34c789ec4de9787401ea6221240
Thanh Nguyen <thanhnt198@gmail.com>
2021-12-01 15:30:24 +0700

    build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55da1f3714e1b38c0c52df94f9547505.js"
+  "/precache-manifest.2c174b77a09c28bee32d1fab859a1e92.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blockchain-web-backoffice"});
