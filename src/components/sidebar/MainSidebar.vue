3538980fa973e3d0394cbdbe968240c34d5dc36b
Thanh Nguyen <thanhnt198@gmail.com>
2021-12-09 15:12:07 +0700

    fix

--- src/components/sidebar/MainSidebar.vue
+++ src/components/sidebar/MainSidebar.vue
@@ -99,7 +99,7 @@
 
       <el-button slot="reference" class="is-create-color is-white is-none-border icon-btn p-0 m-0 button-add" style="height: 100%; position: relative; width: 100%">
         <base-icon icon="icon-exception-bo" style="font-size: 27px" />
-        <span style="font-size: 11px; display: block; margin-top: 7px">Thêm</span>
+        <span style="font-size: 11px; display: block; margin-top: 7px">{{ $t('leftMenu.more') }}</span>
       </el-button>
     </el-popover>
 
