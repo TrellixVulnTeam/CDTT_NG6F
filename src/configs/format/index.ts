3199d0b019e3794c1f0266a6037bafb0479d4788
Datnq <dat@beedu.vn>
2021-12-13 09:43:11 +0700

    fix round

--- src/configs/format/index.ts
+++ src/configs/format/index.ts
@@ -179,6 +179,14 @@ export function formatMMDDYY(value: string | number): any {
   )
 }
 
+export function formatMMDDYYWithoutHour(value: string | number): any {
+  if (!value) {
+    return ''
+  }
+  const date = new Date(value)
+  return (date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '/' + (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + '/' + date.getFullYear()
+}
+
 export function encryptPassword(pass: string): string | boolean {
   const PUBLIC_KEY =
     'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCgw0IeOV0DZL1ZfzsSSbZDRu9XPPBkb4EVsFKYODFJnEEMdVK3IBhCyehoUbJFufIvdQD78LyfRo46fB5B6k+EsLC/4vofQiYyE/R13r+Fd+xufTXpxDKkj544LtfBfvsh/fYFx+vUH1/5XtuCplBX4cOsIdMni026zZv46clJswIDAQAB'
