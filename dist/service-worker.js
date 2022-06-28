130c50453a8f54b9c4f92a5a24d93ce468824153
Dat Nguyen <datnguyen.7031@gmail.com>
2022-06-28 09:23:14 +0700

    build

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.683fb9ee5d2e9d9a32caabb8339f4693.js"
+  "/precache-manifest.a1fafbddbea4073f9cec9daf2d200dd9.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "blockchain-web-backoffice"});
